07f26115bd3a518f7b8466d85f48f3fe17728a97
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-10-04 15:19:01 +0300

    defining DS paths for front and back

--- resources/config.php
+++ resources/config.php
@@ -4,17 +4,14 @@
 */
 defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
 
+/* Defining paths for frontt and back
+*/
+defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "templates/front");
+defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "templates/back");
+
 /* The ROOT of the Ecommerce 
 */
 echo __DIR__;
-
-
-
-
-
-
-
-
 
 
 ?>
