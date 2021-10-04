9af0ee888bbcd35dec3db4583a0fc4edbbd0bf44
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-10-04 15:30:48 +0300

    defining DB paths; adding ob_start; adding session_start

--- resources/config.php
+++ resources/config.php
@@ -1,17 +1,30 @@
 <?php
 
+ob_start();
+
+session_start();
+
 /* \ or / depending on what system you are
 */
 defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
 
-/* Defining paths for frontt and back
+/* Defining paths for front and back
 */
 defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "templates/front");
 defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "templates/back");
 
-/* The ROOT of the Ecommerce 
+/* DB definitions
 */
-echo __DIR__;
+defined("DB_HOST") ? null : define("DB_HOST", "localhost");
+defined("DB_USER") ? null : define("DB_USER", "root");
+defined("DB_PASSWORD") ? null : define("DB_PASSWORD", "");
+defined("DB_NAME") ? null : define("DB_NAME", "ecommerce_db");
 
+/* Connect to the DB
+*/
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+
+require_once("funtions.php");
 
 ?>
