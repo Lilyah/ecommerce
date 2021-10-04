ff35e1f5494201c807abb1a6746f3f75c4b83124
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-10-04 15:05:05 +0300

    setting DIRECTORY_SEPARATOR and __DIR__

---
+++ resources/config.php
@@ -0,0 +1,20 @@
+<?php
+
+/* \ or / depending on what system you are
+*/
+defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
+
+/* The ROOT of the Ecommerce 
+*/
+echo __DIR__;
+
+
+
+
+
+
+
+
+
+
+?>
