475c81027207dc50f84ec74e975c27373e263afe
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-10-05 12:32:57 +0300

    refactoring the index page

--- public/index.php
+++ public/index.php
@@ -15,44 +15,24 @@ include(TEMPLATE_FRONT . "/header.php");
 
         <div class="row">
 
-            <div class="col-md-3">
-                <p class="lead">Shop Name</p>
-                <div class="list-group">
-                    <a href="category.html" class="list-group-item">Category 1</a>
-                    <a href="#" class="list-group-item">Category 2</a>
-                    <a href="#" class="list-group-item">Category 3</a>
-                </div>
-            </div>
+            <?php
+            /* TEMPLATE_FRONT is custom defined in resources/config.php
+            */
+            include(TEMPLATE_FRONT . "/side_nav.php")
+            ?>
 
             <div class="col-md-9">
 
                 <div class="row carousel-holder">
 
                     <div class="col-md-12">
-                        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                            </ol>
-                            <div class="carousel-inner">
-                                <div class="item active">
-                                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
-                                </div>
-                                <div class="item">
-                                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
-                                </div>
-                                <div class="item">
-                                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
-                                </div>
-                            </div>
-                            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                                <span class="glyphicon glyphicon-chevron-left"></span>
-                            </a>
-                            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                                <span class="glyphicon glyphicon-chevron-right"></span>
-                            </a>
-                        </div>
+
+                    <?php
+                    /* TEMPLATE_FRONT is custom defined in resources/config.php
+                    */
+                    include(TEMPLATE_FRONT . "/slider.php")
+                    ?>
+                        
                     </div>
 
                 </div>
