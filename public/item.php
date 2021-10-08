3c5e1ca5a0119e3b34e74fb91b0926b40a4dad67
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-10-08 14:39:24 +0300

    refactoring the footer

--- public/item.php
+++ public/item.php
@@ -211,28 +211,10 @@ include(TEMPLATE_FRONT . "/header.php");
 </div>
     <!-- /.container -->
 
-    <div class="container">
+<?php
 
-        <hr>
+/* TEMPLATE_FRONT is custom defined in resources/config.php
+*/
+include(TEMPLATE_FRONT . "/footer.php");
 
-        <!-- Footer -->
-        <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
-                </div>
-            </div>
-        </footer>
-
-    </div>
-    <!-- /.container -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-</body>
-
-</html>
+?>
