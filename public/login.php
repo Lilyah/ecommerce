55ace3ef2df5314d320d98f1214b541b309250cf
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-10-11 12:55:28 +0300

    setting login_user() and set_message()

--- public/login.php
+++ public/login.php
@@ -13,13 +13,19 @@ include(TEMPLATE_FRONT . "/header.php");
 
       <header>
             <h1 class="text-center">Login</h1>
+            <h2 class="text-center bg-danger"><?php display_message(); ?></h2>
         <div class="col-sm-4 col-sm-offset-5">         
             <form class="" action="" method="post">
+
+                <?php 
+                login_user();
+                ?>
+
                 <div class="form-group"><label for="">
                     username<input type="text" name="username" class="form-control"></label>
                 </div>
                  <div class="form-group"><label for="password">
-                    Password<input type="text" name="password" class="form-control"></label>
+                    Password<input type="text" name="user_password" class="form-control"></label>
                 </div>
 
                 <div class="form-group">
