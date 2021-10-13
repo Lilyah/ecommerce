90aefccdec0c6de6653071bd4db74a03eba2ae22
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-10-13 16:01:37 +0300

    creating thank you page for successful payment

---
+++ public/thank_you.php
@@ -0,0 +1,23 @@
+<?php 
+
+require_once("../resources/config.php");
+
+include(TEMPLATE_FRONT .  "/header.php");
+
+?>
+
+    <div class="container">
+		<h1 class="text-center" style="margin-top:50px">THANK YOU!</h1>
+
+    </div>
+
+	<div class="main-content text-center">
+    <span class="glyphicon glyphicon-ok text-success" style="font-size: 400%; margin-top: 20px; margin-bottom: 20px"></span>
+    <p class="text-center">Thank you for your purchase. <br>We already send you confirmation email with details of your order.</p>
+	</div>
+
+    <?php 
+
+include(TEMPLATE_FRONT .  "/footer.php");
+
+?>
