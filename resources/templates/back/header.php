fa48b73330e55d452b0ef14331fbfbd9524db1c9
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-10-15 12:14:49 +0300

    separating side navigation in different file

--- resources/templates/back/header.php
+++ resources/templates/back/header.php
@@ -46,26 +46,10 @@
             ?>
 
             <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav side-nav">
-                    <li class="active">
-                        <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="products.html"><i class="fa fa-fw fa-bar-chart-o"></i> View Products</a>
-                    </li>
-                    <li>
-                        <a href="add_product.html"><i class="fa fa-fw fa-table"></i> Add Product</a>
-                    </li>
-                    
-                    <li>
-                        <a href="categories.html"><i class="fa fa-fw fa-desktop"></i> Categories</a>
-                    </li>
-                    <li>
-                        <a href="users.html"><i class="fa fa-fw fa-wrench"></i>Users</a>
-                    </li>
-                
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
+            
+            <?php
+
+            include(TEMPLATE_BACK . "/side_nav.php");
+
+            ?>
         </nav>
