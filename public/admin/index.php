b591d356e10905f064989a8504eef033ac8e57fb
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-10-15 11:31:19 +0300

    refactoring the footer

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,7 +1,7 @@
 <?php 
 
 require_once("../../resources/config.php");
-include(TEMPLATE_BACK . "/header.php");
+include(TEMPLATE_BACK  . "/header.php");
 
 ?>
 
@@ -293,20 +293,9 @@ include(TEMPLATE_BACK . "/header.php");
         </div>
         <!-- /#page-wrapper -->
 
-    </div>
-    <!-- /#wrapper -->
+   
+<?php 
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+include(TEMPLATE_BACK  . "/footer.php");
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Morris Charts JavaScript -->
-    <script src="js/plugins/morris/raphael.min.js"></script>
-    <script src="js/plugins/morris/morris.min.js"></script>
-    <script src="js/plugins/morris/morris-data.js"></script>
-
-</body>
-
-</html>
+?>
