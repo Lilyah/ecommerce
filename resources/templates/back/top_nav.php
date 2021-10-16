fd5d3c08b05f30e0bd1a515d111a0f1a1ece6453
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-10-16 10:22:40 +0300

    echo the username for loged in users

--- resources/templates/back/top_nav.php
+++ resources/templates/back/top_nav.php
@@ -5,13 +5,19 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="index.html">SB Admin</a>
+    <a class="navbar-brand" href="index">SB Admin</a>
 </div>
 
 <!-- Top Menu Items -->
 <ul class="nav navbar-right top-nav">
     <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>&nbsp;
+            <?php 
+            if(isset($_SESSION['username'])){
+                echo $_SESSION['username'];
+            }
+            ?>
+        <b class="caret"></b></a>
         <ul class="dropdown-menu">
                        
             <li class="divider">
