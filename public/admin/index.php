0668685f659aa952fdb1e885df060c6967af9c38
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-10-18 12:50:56 +0300

    moving the header from index into admin_content

--- public/admin/index.php
+++ public/admin/index.php
@@ -15,22 +15,6 @@ if(!isset($_SESSION['username'])){
 
             <div class="container-fluid">
 
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                            Dashboard <small>Statistics Overview</small>
-                        </h1>
-                        <h2 class="text-center bg-success"><?php display_message(); ?></h2>
-                        <ol class="breadcrumb">
-                            <li class="active">
-                                <i class="fa fa-dashboard"></i> Dashboard
-                            </li>
-                        </ol>
-                    </div>
-                </div>
-                <!-- /.row -->
-
                 <?php
 
                 if($_SERVER['REQUEST_URI'] == "/ecommerce/public/admin/index"){
