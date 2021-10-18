c024e51641d881dbca5d51976313ccdf0407bea8
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-10-18 14:41:09 +0300

    adding display_message() for successfully deleted product

--- resources/templates/back/products.php
+++ resources/templates/back/products.php
@@ -8,6 +8,10 @@
                   All Products
                 </h1>
 
+                <h4 class="text-center bg-success">
+                  <?php display_message(); ?>
+                </h4>
+
                 <table class="table table-hover">
                   <thead>
 
