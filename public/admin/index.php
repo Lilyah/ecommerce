400841cac0d4f65e30090dda1ff2c0ed0a619a29
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-10-18 14:34:46 +0300

    adding path for edit product

--- public/admin/index.php
+++ public/admin/index.php
@@ -29,6 +29,10 @@ if(!isset($_SESSION['username'])){
                     include(TEMPLATE_BACK . "/add_product.php");
                 }
 
+                if(isset($_GET['edit_product'])){
+                    include(TEMPLATE_BACK . "/edit_product.php");
+                }
+
                 if(isset($_GET['products'])){
                     include(TEMPLATE_BACK . "/products.php");
                 }
