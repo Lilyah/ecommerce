39cc34859d9fc16c1854e834e0fcf72bb6a7770b
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-10-18 18:05:04 +0300

    cleaning and organizing the html

--- resources/templates/back/add_product.php
+++ resources/templates/back/add_product.php
@@ -5,50 +5,45 @@
    Add Product
 
 </h1>
+<?php add_product() ?>
 </div>
                
 
 
-<form action="" method="post" enctype="multipart/form-data">
+<form action="" method="post" enctype="multipart/form-data"> <!-- enctype="multipart/form-data" is for multifile data -->
 
 
-<div class="col-md-8">
-
-<div class="form-group">
-    <label for="product-title">Product Title </label>
-        <input type="text" name="product_title" class="form-control">
-       
-    </div>
-
+  <div class="col-md-8">
 
     <div class="form-group">
-           <label for="product-title">Product Description</label>
-      <textarea name="product_description" id="" cols="30" rows="10" class="form-control"></textarea>
+      <label for="product-title">Product Title </label>
+      <input type="text" name="product_title" class="form-control"> 
     </div>
 
-
-
     <div class="form-group row">
-
       <div class="col-xs-3">
         <label for="product-price">Product Price</label>
-        <input type="number" name="product_price" class="form-control" size="60">
+        <input type="text" name="product_price" class="form-control" size="60">
       </div>
     </div>
 
+    <div class="form-group">
+      <label for="product-title">Product Short Description</label>
+      <textarea name="short_desc" id="" cols="30" rows="5" class="form-control"></textarea>
+    </div>
+
+    <div class="form-group">
+      <label for="product-title">Product Description</label>
+      <textarea name="product_description" id="" cols="30" rows="10" class="form-control"></textarea>
+    </div>
+
+  </div><!--Main Content-->
 
 
-
-    
-    
-
-</div><!--Main Content-->
+  <!-- SIDEBAR-->
 
 
-<!-- SIDEBAR-->
-
-
-<aside id="admin_sidebar" class="col-md-4">
+  <aside id="admin_sidebar" class="col-md-4">
 
      
      <div class="form-group">
@@ -56,55 +51,30 @@
         <input type="submit" name="publish" class="btn btn-primary btn-lg" value="Publish">
     </div>
 
-
-     <!-- Product Categories-->
-
     <div class="form-group">
-         <label for="product-title">Product Category</label>
-          <hr>
-        <select name="product_category" id="" class="form-control">
+        <label for="product-title">Product Category</label>
+        <select name="product_category_id" id="" class="form-control">
             <option value="">Select Category</option>
-           
         </select>
-
-
-</div>
-
-
-
-
-
-    <!-- Product Brands-->
-
-
-    <div class="form-group">
-      <label for="product-title">Product Brand</label>
-         <select name="product_brand" id="" class="form-control">
-            <option value="">Select Brand</option>
-         </select>
     </div>
 
 
-<!-- Product Tags -->
-
-
     <div class="form-group">
+      <label for="product-title">Product Quantity</label>
+        <input type="number" name="product_quantity" class="form-control">
+    </div>
+
+    <!-- <div class="form-group">
           <label for="product-title">Product Keywords</label>
           <hr>
         <input type="text" name="product_tags" class="form-control">
-    </div>
+    </div> -->
 
-    <!-- Product Image -->
     <div class="form-group">
         <label for="product-title">Product Image</label>
         <input type="file" name="file">
-      
     </div>
 
-
-
-</aside><!--SIDEBAR-->
-
-
-    
+  </aside><!--SIDEBAR-->
+  
 </form>
