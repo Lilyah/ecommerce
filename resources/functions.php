0d2ddfad95d82c57311043164ee3458e2ce4bab5
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-10-19 10:12:26 +0300

    changing href for Buy now btn in get_products_in_shop_page()

--- resources/functions.php
+++ resources/functions.php
@@ -189,7 +189,7 @@ function get_products_in_shop_page(){
                         <h3>{$row['product_title']}</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                         <p>
-                            <a href="item.php?id={$row['product_id']}" class="btn btn-primary">Buy Now!</a> <a href="item.php?id={$row['product_id']}" class="btn btn-default">More Info</a>
+                            <a href="../resources/cart.php?add={$row['product_id']}" class="btn btn-primary">Buy Now!</a> <a href="item.php?id={$row['product_id']}" class="btn btn-default">More Info</a>
                         </ p>
                     </div>
                 </div>
