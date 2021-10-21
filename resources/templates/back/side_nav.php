6f476ef1a542442cccba1fcc42b4c1c2b0cbe916
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-10-21 17:54:09 +0300

    changing some icons and adding <li> for reports

--- resources/templates/back/side_nav.php
+++ resources/templates/back/side_nav.php
@@ -4,19 +4,22 @@
             <a href="index"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
         </li>
         <li>
-            <a href="index?orders"><i class="fa fa-fw fa-dashboard"></i> Orders</a>
+            <a href="index?orders"><i class="fa fa-inbox"></i> Orders</a>
         </li>
         <li>
-            <a href="index?products"><i class="fa fa-fw fa-bar-chart-o"></i> View Products</a>
+            <a href="index?reports"><i class="fa fa-fw fa-bar-chart-o"></i> Reports</a>
+        </li>
+        <li>
+            <a href="index?products"><i class="fa fa-file-text-o"></i> View Products</a>
         </li>
         <li>
             <a href="index?add_product"><i class="fa fa-fw fa-table"></i> Add Product</a>
         </li>
         <li>
-            <a href="index?categories"><i class="fa fa-fw fa-desktop"></i> Categories</a>
+            <a href="index?categories"><i class="fa fa-bars"></i> Categories</a>
         </li>
         <li>
-            <a href="index?users"><i class="fa fa-fw fa-wrench"></i>Users</a>
+            <a href="index?users"><i class="fa fa-group"></i> Users</a>
         </li>
     
     </ul>
