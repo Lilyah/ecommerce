e611983edbb310b710b26f7820ec3dcb3dfd5bd3
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-10-24 19:08:20 +0300

    adding functionality for deleting the file from uploads directory

--- resources/templates/back/delete_slide.php
+++ resources/templates/back/delete_slide.php
@@ -3,8 +3,22 @@
 require_once("../../config.php");
 
 if(isset($_GET['id'])){
-    $query = query("DELETE FROM slides WHERE slide_id = " . escape_string($_GET['id']) . " ");
-    confirm($query);
+
+    /* Deleting the file from the folder 
+    */
+    /* First we are pulling the file name */
+    $pull_title_query = query("SELECT slide_image FROM slides WHERE slide_id = " . escape_string($_GET['id']) . " ");
+    confirm($pull_title_query);
+
+    /* Second we are finding the file and deleting it with unlink */
+    $row = fetch_array($pull_title_query);
+    $path_to_file = UPLOAD_DIRECTORY . DS . $row['slide_image'];
+    unlink($path_to_file);
+
+    /* Deleting the record from the DB */
+    $delete_query = query("DELETE FROM slides WHERE slide_id = " . escape_string($_GET['id']) . " ");
+    confirm($delete_query);
+
 
     set_message("Banner deleted successfully");
     redirect("../../../public/admin/index?slides");
