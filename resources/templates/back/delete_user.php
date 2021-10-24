d95c198f22fa06d268fb7ed2d598d219192885b2
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-10-24 19:16:52 +0300

    adding functionality for deleting the user photo file from uploads directory

--- resources/templates/back/delete_user.php
+++ resources/templates/back/delete_user.php
@@ -3,6 +3,19 @@
 require_once("../../config.php");
 
 if(isset($_GET['id'])){
+
+    /* Deleting the file from the upload folder 
+    */
+    /* First we are pulling the file name */
+    $pull_title_query = query("SELECT user_photo FROM users WHERE user_id = " . escape_string($_GET['id']) . " ");
+    confirm($pull_title_query);
+
+    /* Second we are finding the file and deleting it with unlink */
+    $row = fetch_array($pull_title_query);
+    $path_to_file = UPLOAD_DIRECTORY . DS . $row['user_photo'];
+    unlink($path_to_file);
+
+    /* Deleting the record from the DB */
     $query = query("DELETE FROM users WHERE user_id = " . escape_string($_GET['id']) . " ");
     confirm($query);
 
